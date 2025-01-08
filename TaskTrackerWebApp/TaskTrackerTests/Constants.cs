c0f6e73fbcecfab495b04d67517bd20c49a58ab3
FilipPopescu <filippopescu18@gmail.com>
2025-01-08 13:02:02 +0200

    Added proper tests for Login page

---
+++ TaskTrackerWebApp/TaskTrackerTests/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskTrackerTests
+{
+    public static class Constants
+    {
+        public const string Root = "http://localhost:5273";
+        public const string Login = "login";
+    }
+}
---
+++ TaskTrackerWebApp/TaskTrackerTests/LoginTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Playwright.MSTest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using TaskTrackerTests;
+
+namespace PlaywrightTests;
+
+[TestClass]
+public class LoginTest : PageTest
+{
+    [TestInitialize]
+    public async Task Init()
+    {
+        await Page.GotoAsync($"{Constants.Root}/{Constants.Login}");
+    }
+
+
+    [TestMethod]
+    public async Task LoginWithValidCredentials()
+    {
+        await Page.FillAsync("input[placeholder='Username']", "eu");
+        await Page.FillAsync("input[placeholder='Password']", "pass");
+
+        await Page.ClickAsync("button[type='submit']");
+
+        await Expect(Page).ToHaveURLAsync(new Regex($"{Constants.Root}"));
+    }
+
+    [TestMethod]
+    public async Task LoginWithInvalidCredentials()
+    {
+        await Page.FillAsync("input[placeholder='Username']", "invalidUsername");
+        await Page.FillAsync("input[placeholder='Password']", "invalidPassword");
+
+        await Page.ClickAsync("button[type='submit']");
+
+        var errorMessage = Page.Locator(".text-danger", new() { HasText = "Invalid username or password" });
+        await Expect(errorMessage).ToBeVisibleAsync();
+    }
+}
