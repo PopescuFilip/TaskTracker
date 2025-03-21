c004e8bbb5179d0316e532c5d60b376f644ba7b3
FilipPopescu <filippopescu18@gmail.com>
2025-03-21 15:43:11 +0200

    Added docker compose for UserAPI

--- TaskTrackerWebApp/UserAPI/Db/UserTrakerDbContextFactory.cs
+++ TaskTrackerWebApp/UserAPI/Db/UserTrakerDbContextFactory.cs
@@ -4,15 +4,20 @@ namespace UserAPI.Db
 {
     public class UserTrakerDbContextFactory: IDbContextFactory<UserTrakerDbContext>
     {
+        private readonly string _connectionString;
+
+        public UserTrakerDbContextFactory()
+        {
+            var dbHost = Environment.GetEnvironmentVariable("DB_HOST") ?? "localhost";
+            var dbName = Environment.GetEnvironmentVariable("DB_NAME") ?? "dms_user";
+            var dbPass = Environment.GetEnvironmentVariable("DB_SA_PASSWORD") ?? "m153cur3p@55";
+            _connectionString = $"Data Source={dbHost};Initial Catalog={dbName};User=sa;Password={dbPass};TrustServerCertificate=True;";
+        }
+
         public UserTrakerDbContext CreateDbContext()
         {
-            var dbHost = "localhost";
-            var dbName = "dms_user";
-            var dbPass = "m153cur3p@55";
-            var connString = $"Data Source={dbHost};Initial Catalog={dbName};User=sa;Password={dbPass};TrustServerCertificate=True;";
-
             var options = new DbContextOptionsBuilder<UserTrakerDbContext>()
-                .UseSqlServer(connString)
+                .UseSqlServer(_connectionString)
                 .Options;
 
             return new UserTrakerDbContext(options);
