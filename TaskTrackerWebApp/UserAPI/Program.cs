8f3e619ddc1c0381817f075c2b1c6b2dfe31178c
FilipPopescu <filippopescu18@gmail.com>
2025-03-21 12:26:32 +0200

    Fixed UserAPI removed reference to TaskAPI

--- TaskTrackerWebApp/UserAPI/Program.cs
+++ TaskTrackerWebApp/UserAPI/Program.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using UserAPI.Db;
 using UserAPI.Services;
 
@@ -8,8 +9,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+//builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 
 builder.Services.AddSingleton<IUserService, UserService>();
 
